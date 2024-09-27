f1137776c9b9c964dba61c53986b8a2af77735a5
Vinicius Gabriel <146395205+gitviini@users.noreply.github.com>
2024-09-27 00:42:37 +0000

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, json, request
 
 app = Flask(__name__)
 
--- app/controlers/default.py
+++ app/controlers/default.py
@@ -1,5 +1,5 @@
 # importando
-from app import app, render_template
+from app import app, render_template, json, jsonify, request as req
 
 infos = [
     'version : 1.0.0',
@@ -9,5 +9,7 @@ infos = [
 # Rota inicial
 @app.route("/")
 def home():
+    if req.method == 'POST':
+        print(req.form)
     # Retornando html e informações
     return render_template("index.html", infos=infos)
