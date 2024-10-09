43237dc70a3e636d19b37aa62d5cbff5ec0de9c4
gitviini <gvinicius105@gmail.com>
2024-10-08 22:58:37 -0300

    initial page create

---
+++ project/app/pages/page.tsx
@@ -0,0 +1,13 @@
+export default function Chat(){
+    return (
+        <main>
+            <header>
+                <nav>
+                    <ul>
+                        
+                    </ul>
+                </nav>
+            </header>
+        </main>
+    )
+}
