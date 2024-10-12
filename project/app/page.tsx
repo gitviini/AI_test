fc052dee32be8b2b38342834cc348294e283b3c3
Vinicius Gabriel <146395205+gitviini@users.noreply.github.com>
2024-10-12 00:22:09 +0000

    pages/api/routes rest created

--- project/app/page.tsx
+++ project/app/page.tsx
@@ -1,12 +1,15 @@
+import { NextRequest } from "next/server"
+
 export default function Home() {
+  let req = NextRequest
   return (
     <main
       className="flex flex-1 flex-col gap-2 items-center justify-center font-mono h-screen">
       <div className="flex flex-col items-start gap-4">
-        <h1 className="text-4xl font-bold">
-          HI,<br /><span className="text-white/[.5]">AI bot here!</span>
+        <h1 className="text-5xl font-bold">
+          Hi,<br /><span className="text-white/[.5]">AI bot here!</span>
         </h1>
-        <ol className="list-inside list-decimal">
+        <ol className="list-inside list-decimal text-lg">
           <li>Tudo começa com uma ideia.</li>
           <li>E termina com um produto.</li>
         </ol>
--- project/app/pages/page.tsx
+++ project/app/pages/page.tsx
@@ -1,16 +1,18 @@
+'use client';
 import HeaderDefault from "@/components/Header";
-import Link from "next/link";
+import { useState } from "react";
 
 export default function Chat() {
+    const [prompt, setPrompt] = useState("")
     return (
         <main className="font-mono h-screen flex flex-col justify-between items-center">
             <HeaderDefault />
             <div className="flex flex-col justify-center items-center p-2">
                 <form action="" method="post"
                     className="flex flex-row gap-2">
-                    <input className="p-2 rounded-md focus:outline-none hover:-translate-y-1 focus:-translate-y-0 text-black transition duration-200 w-screen max-w-sm min-w-5"
-                        type="text" required placeholder="Prompt" />
-                    <button className="p-2 bg-sky-800 rounded-md hover:-translate-y-1 focus:-translate-y-0 transition duration-200">enviar</button>
+                    <input className="bg-yellow-50 text-black outline-none p-2 rounded-md transition duration-200 w-screen max-w-sm min-w-5"
+                        type="text" required placeholder="Prompt" onChange={e=>setPrompt(e.target.value)} />
+                    <button className="p-2 bg-sky-800 rounded-md transition duration-200">enviar</button>
                 </form>
             </div>
         </main>
